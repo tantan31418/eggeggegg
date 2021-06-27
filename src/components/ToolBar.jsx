9c1c3c2395e85091d310e28fda128944206d5f13
yingcheng <twin.sisc@gmail.com>
2021-06-28 01:53:33 +0800

    Update UI

--- src/components/ToolBar.jsx
+++ src/components/ToolBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ToolBarButton from './ToolBarButton';
 import KillButton from '../components/KillButton.jsx';
+import './ToolBar.css';
 
 
 
@@ -17,7 +18,7 @@ export default class ToolBar extends React.Component{
 
     render(){
         return (
-            <div>
+            <div className='toolbar'>
                 <div className="d-flex justify-content-between">
                 <ToolBarButton buttonType="Settings"/>
                 <KillButton/>
