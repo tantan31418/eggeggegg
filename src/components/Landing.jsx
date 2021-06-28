ebb41bdad75645fa78d1684f42f838805a020ba5
Tang <tantan3141@gapp.nthu.edu.tw>
2021-06-28 19:16:23 +0800

    Hide button when new_egg

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -44,28 +44,34 @@ export default class Landing extends React.Component{
                     current_animal_id={this.props.current_animal_id}
                 />
             <div className="d-flex justify-content-center land-center">
-                <div>
-                    {/* <p>{auth.getCurrentUser}</p> */}
-                    <p id='think'>想不到開心的事嗎？</p>
-                    {/* <button onClick={this.props.foo_update}>foo update</button> */}
-                    <a href="/other_happy">看看別人因為什麼感到開心...</a>
-                    {this.props.status === 'new_egg' ? 
-                    <CreateAni create_animal={this.props.create_animal}/>
-                    :
-                     <EggHero current_animal={this.props.current_animal} status={this.props.status}/>}
-                    <TdRcCount count={this.props.today_recorded}/>
-                    <div className="d-flex justify-content-center">
-                        <PostModal id={this.props.id} 
-                        today_recorded={this.props.today_recorded}
-                        updateUser={this.props.updateUser}
-                        cur_ani_create_date={this.props.cur_ani_create_date}
-                        user_status={this.props.status}
-                        current_animal={this.props.current_animal}
-                        collection={this.props.collection}
-                        current_animal_id={this.props.current_animal_id}
-                        score={this.props.score}
-                        />
-                    </div>
+                    <div>
+                        {/* <p>{auth.getCurrentUser}</p> */}
+                        <p id='think'>想不到開心的事嗎？</p>
+                        {/* <button onClick={this.props.foo_update}>foo update</button> */}
+                        <a href="/other_happy">看看別人因為什麼感到開心...</a>
+                        {this.props.status === 'new_egg' ?
+                            <div>
+                            <CreateAni create_animal={this.props.create_animal} />
+                            <h3>選擇一個動物</h3>
+                            </div>
+                            :
+                            <div>
+                                <EggHero current_animal={this.props.current_animal} status={this.props.status} />
+                                <TdRcCount count={this.props.today_recorded} />
+                                <div className="d-flex justify-content-center">
+                                    <PostModal id={this.props.id}
+                                        today_recorded={this.props.today_recorded}
+                                        updateUser={this.props.updateUser}
+                                        cur_ani_create_date={this.props.cur_ani_create_date}
+                                        user_status={this.props.status}
+                                        current_animal={this.props.current_animal}
+                                        collection={this.props.collection}
+                                        current_animal_id={this.props.current_animal_id}
+                                        score={this.props.score}
+                                    />
+                                </div>
+                            </div>
+    }
                     
                     <ScoreBoard todaysscore={this.props.score.today_score} weeklyscore={this.props.score.week_score} 
                     monthlyscore={this.props.score.month_score} historyscore={this.props.score.history_score}
